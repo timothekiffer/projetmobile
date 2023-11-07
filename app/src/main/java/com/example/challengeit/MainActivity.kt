550855912a06ee0f1cdb3e76f7c35dbcb549da01
timothekiffer <timothe.kiffer@gmail.com>
2023-11-07 18:44:47 -0500

    refix project from 0

--- app/src/main/java/com/example/challengeit/MainActivity.kt
+++ app/src/main/java/com/example/challengeit/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.challengeit.ui.theme.ChallengeITTheme
+import com.example.challengeit.ui.theme.ChallengeItTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ChallengeITTheme {
+            ChallengeItTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ChallengeITTheme {
+    ChallengeItTheme {
         Greeting("Android")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Challenge IT"
+rootProject.name = "ChallengeIt"
 include(":app")
