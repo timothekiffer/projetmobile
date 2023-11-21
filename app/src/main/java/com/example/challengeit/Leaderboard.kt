6784a854bf9f3f9e0564acbf96dd52f84f5694c7
timothekiffer <timothe.kiffer@gmail.com>
2023-11-21 18:45:33 -0500

    refacto #2

--- app/src/main/java/com/example/challengeit/Leaderboard.kt
+++ app/src/main/java/com/example/challengeit/Leaderboard.kt
@@ -1,8 +1,5 @@
 package com.example.challengeit
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -20,9 +17,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,46 +27,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.challengeit.ui.theme.ChallengeItTheme
-
-class LeaderboardActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ChallengeItTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = Screen.Leaderboard.route){
-                        composable(Screen.Leaderboard.route) {
-                            val users = listOf(
-                                User(id = 1, name = "Timothé", point = 150),
-                                User(id = 2, name = "Alexandre", point = 89),
-                                User(id = 3, name = "Romain", point = 18)
-                            )
-                            LeaderboardScreen(users, navController)
-                        }
-                        composable(Screen.Group.route) {
-                            val challenges = listOf(
-                                Challenge(id = 1, name = "Faire 300 pas en 1 minute", description = "Pour valider le défi, tu dois faire 1000 pas en 1 minute, cela devra être filmé et uploadé sur l’appli", point = 5),
-                                Challenge(id = 2, name = "Prendre un selfie devant la Tour Eiffel", description = "", point = 30),
-                                Challenge(id = 3, name = "Prendre un bain de minuit", description = "", point = 10),
-                                Challenge(id = 4, name = "Danser la macarena sur une place publique", description = "", point = 20)
-                            )
-                            GroupScreen(challenges, navController)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
 
 data class User(val id: Int, val name: String, val point: Int)
 
--- app/src/main/java/com/example/challengeit/NewGroup.kt
+++ app/src/main/java/com/example/challengeit/NewGroup.kt
@@ -1,8 +1,5 @@
 package com.example.challengeit
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,7 +10,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -25,40 +21,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.challengeit.ui.theme.ChallengeItTheme
-
-class NewGroupActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ChallengeItTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = Screen.NewGroup.route){
-                        composable(Screen.NewGroup.route) {
-                            NewGroupScreen(navController)
-                        }
-                        composable(Screen.MainPage.route) {
-                            val groups = listOf(
-                                Group(name = "Groupe UQAC"),
-                                Group(name = "Groupe 2"),
-                                Group(name = "Groupe 3")
-                            )
-                            MainPageScreen(groups, navController)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
